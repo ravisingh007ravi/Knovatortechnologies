ea424398a5fa8012787644e795dc3b31508eac81
Ravi Singh <ravisingh007ravi@gmail.com>
2024-05-10 18:34:36 +0530

    add same validation in backend

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const route = require('./route/routes.js');
 const app=express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3001;
 
 
 const url ="mongodb+srv://ravi6680singh:R%40vi786R%40vi@cluster0.viocq1u.mongodb.net/Knovatortechnologies=true&w=majority";
